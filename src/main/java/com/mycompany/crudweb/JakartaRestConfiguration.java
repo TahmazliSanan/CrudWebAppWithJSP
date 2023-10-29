c0c1a5487a8276ecd384f14eac1e59a58ea6daae
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-10-30 01:07:02 +0400

    Create project

---
+++ src/main/java/com/mycompany/crudweb/JakartaRestConfiguration.java
@@ -0,0 +1,8 @@
+package com.mycompany.crudweb;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+}
